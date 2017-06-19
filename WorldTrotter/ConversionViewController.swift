6eb0f47f344e8e2c460bd1c01cd396a243dcb3aa
StefanKollaart <garbera@icloud.com>
2017-06-19 20:09:36 +0200

    basic template, basic logic

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Stefan Kollaart on 19-06-17.
+//  Copyright © 2017 Stefan Kollaart. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    let letters = CharacterSet.letters
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
+    
+
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        var isAlphabetical = false
+        if let newString = UnicodeScalar(string) {
+            if letters.contains(newString) {
+                isAlphabetical = true
+            }
+        }
+        
+        print(isAlphabetical)
+
+        if (existingTextHasDecimalSeperator != nil && replacementTextHasDecimalSeperator != nil) || isAlphabetical {
+            print("hallo")
+            return false
+        } else {
+            print("hoi")
+            return true
+        }
+    }
+}
